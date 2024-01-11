69f410204e20962062d03a9f08a75f38d83b1598
Jose Plaud <joseenrique1031@gmail.com>
2024-01-11 01:05:43 -0400

    Submission

---
+++ IOS PreWork/IOS PreWork/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  IOS PreWork
+//
+//  Created by Jose Plaud on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
